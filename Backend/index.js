ba7c2cbeb8805e1c2206c1f197d0bf52a8e5ad4a
Rajasah2000 <rajasah30030@gmail.com>
2024-10-02 22:58:33 +0530

    port change

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const connectDb = require('./config/db');
-const authRoutes = require('./config/routes/authRoutes')
+const express = require("express");
+const connectDb = require("./config/db");
+const authRoutes = require("./config/routes/authRoutes");
 
 const app = express();
-const port = 3000;
+const port = 8025;
 
 //  Middleware for passing JSON
 app.use(express.json());
 
-app.use('/api', authRoutes);
-
+app.use("/api", authRoutes);
 
 connectDb().then(() => {
-    app.listen(port, () =>{
-        console.log(`Server is running on port : ${port}`);
-    })
-})
+  app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+  });
+});
